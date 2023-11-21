15f551482f7c9fabdfe18d9a6b3a40231019e5b3
Felix Lahnsteiner <felix.lahnsteiner@outlook.com>
2023-11-21 14:43:55 +0100

    enable `./gradlew quarkusDev`

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,19 +1,4 @@
-CREATE TABLE measurement(
-                timestamp TIMESTAMPTZ NOT NULL,
-                device_id varchar(255),
-                current_l1a float,
-                current_l2a float,
-                current_l3a float,
-                voltage_l1v float,
-                voltage_l2v float,
-                voltage_l3v float,
-                instantaneous_active_power_plus_w float,
-                instantaneous_active_power_minus_w float,
-                total_energy_consumed_wh float,
-                total_energy_delivered_wh float,
-                PRIMARY KEY (timestamp, device_id)
-                );
-
-CREATE UNIQUE INDEX idx_timestamp_device_id ON measurement(device_id, timestamp);
-
 SELECT create_hypertable('measurement', 'timestamp',  partitioning_column => 'device_id', number_partitions => 10);
+
+INSERT INTO public.devicecategory (category_name, description) VALUES ('washing_machine', 'felix');
+INSERT INTO public.measurement (current_l1a, current_l1v, current_l2a, current_l2v, current_l3a, current_l3v, instantaneous_active_power_minus_w, instantaneous_active_power_plus_w, total_energy_consumed_wh, total_energy_delivered_wh, timestamp, device_id) VALUES (1, 0, 0, 1, 1, 1, 1, 1, 1, 1, '2023-11-19 11:59:17.000000', '1');
